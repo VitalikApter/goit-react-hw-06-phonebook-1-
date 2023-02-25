2e01259c0781772d11a7f0ccb5af3f5f7b8a25a8
VitalikApter <aptvetal2014@gmail.com>
2023-02-25 20:13:45 +0200

    Надо логику перенести по компонентам

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,16 @@
 import PhoneBook from 'components/PhoneBook/PhoneBook';
-import { Store, persistor } from 'redux/Store';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+
+
 
 export const App = () => {
   return (
-    <Provider store={Store}>
-      <PersistGate loading={null} persistor={persistor}>
+    
+      
         <div>
           <PhoneBook />
         </div>
-      </PersistGate>
-    </Provider>
+      
+    
   );
 };
 
